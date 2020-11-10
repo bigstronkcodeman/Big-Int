5d19016b243abf12e7498a935d4566b9e8e01986
Phillip Boudreau <43645849+bigstronkcodeman@users.noreply.github.com>
2020-11-10 00:57:38 -0600

    Update BigInt.cpp

--- BigInt.cpp
+++ BigInt.cpp
@@ -4,7 +4,7 @@
    *              BIGINT CLASS METHODS               *
    ***************************************************  */
 
-// Default constructor
+   // Default constructor
 BigInt::BigInt()
 	: digits(std::vector<uint>(1, 0))
 	, positive(true)
@@ -53,6 +53,7 @@ template <typename T> void BigInt::init(T _num) {
 	for (_long n = num; n > 0; n /= BASE) {
 		digits.push_back(n % BASE);
 	}
+	if (digits.size() == 0) digits.push_back(0);
 }
 
 // return binary string representation of BigInt
@@ -94,16 +95,16 @@ std::string BigInt::to_string() const {
 	const size_t num_digits = this->num_digits();
 	std::vector<uint> bcd((BITS_IN_NIBBLE * (log10(2) * num_digits * BITS_IN_UINT + 1)) / BITS_IN_UINT + 1, 0);
 
-	// perform double-dabble algorithm
+	// perform double-dabble algorithm to convert *this from binary to BCD
 	for (_long i = num_digits - 1; i >= 0; --i) { // for each uint digit in this BigInt's digits (going from MSD -> LSD)
 		uint digit = this->digits[i];
 
-		uint bit_iters = 0; // want to keep track of how many bits we've iterated through on current uint digit to shift down to LSB accordingly after masking
+		uint bit_iters = 0; // want to keep track of how many bits we've iterated through on current uint digit to shift down to LSB after masking
 		for (uint mask = 1 << (BITS_IN_UINT - 1); mask > 0; mask >>= 1) { // for each bit in digit (going from MSB -> LSB)
 			uint insert_bit = (digit & mask) >> (BITS_IN_UINT - 1 - bit_iters++); // grab bit, shift down to LSB 
 
 			for (_long j = bcd.size() - 1; j >= 0; --j) { // for each bcd byte
-				if (bcd[j] != 0) { 
+				if (bcd[j] != 0) {
 					dabble(bcd[j]); // dabble all nibbles in uint digit if digit is nonzero
 				}
 			}
@@ -111,7 +112,7 @@ std::string BigInt::to_string() const {
 			uint lmb_mask = 1 << (BITS_IN_UINT - 1); // mask for getting the leftmost bit of a uint digit
 			for (_long k = bcd.size() - 1; k >= 0; --k) { // for each bcd uint digit
 				if (k == 0 || bcd[k - 1] != 0) {
-					uint lmb = (k > 0) ? (bcd[k - 1] & lmb_mask) >> (BITS_IN_UINT - 1) : insert_bit; // grab leftmost bit from right adjacent uint digit OR use insert_bit from non-bcd because we're at the ones column
+					uint lmb = (k > 0) ? (bcd[k - 1] & lmb_mask) >> (BITS_IN_UINT - 1) : insert_bit; // grab leftmost bit from right adjacent uint digit OR use insert_bit from non-bcd if we've reached the ones column
 					bcd[k] = (bcd[k] << 1) | lmb; // insert the desired bit as LSB
 				}
 			}
@@ -120,26 +121,25 @@ std::string BigInt::to_string() const {
 
 	// build decimal string from bcd digit vector
 	const char nibble_map[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }; // just makes life a little easier
-	std::string bcd_str = "";
+	std::string decimal_str = "0";
 	bool leading_zeros = true; // want to flag when leading zeros end so they can be excluded
 	for (_long i = bcd.size() - 1; i >= 0; --i) { // for each bcd uint digit 
 		uint nibble_iters = 0; // want to keep track of how many nibbles have been iterated through in current digit so we can shift accordingly
 		for (uint mask = 0xF << (BITS_IN_UINT - BITS_IN_NIBBLE); mask > 0; mask >>= BITS_IN_NIBBLE) { // for every nibble in this uint digit (going from MSN -> LSN)
 			char digit = nibble_map[(bcd[i] & mask) >> ((BITS_IN_UINT - BITS_IN_NIBBLE) - nibble_iters)]; // get nibble, shift down to the four LSBs, use as index for nibble map to get digit character
 			if (!leading_zeros) {
-				bcd_str.push_back(digit); // append the digit if there are no more leading zeros
+				decimal_str.push_back(digit); // append the digit if there are no more leading zeros
 			}
-			else {
-				if (digit != '0') {
-					bcd_str.push_back(digit); // append the first non-zero digit and flag that leading zeros are done
-					leading_zeros = false;
-				}
+			else if (digit != '0') {
+				decimal_str.pop_back();
+				decimal_str.push_back(digit); // append the first non-zero digit and flag that leading zeros are done
+				leading_zeros = false;
 			}
 			nibble_iters += BITS_IN_NIBBLE;
 		}
 	}
 
-	return bcd_str;
+	return decimal_str;
 }
 
 // get list of digits
@@ -159,7 +159,7 @@ std::vector<bool> BigInt::get_bits(uint num, bool pad_uint) {
 		num >>= 1; // shift num right 1 bit, now what was the second to last bit is last bit
 	}
 
-	if (pad_uint) { // pad vector with 0s if they want 32-bit number
+	if (pad_uint) { // pad vector with 0s if we want 32-bit number
 		for (size_t i = res_bits.size(); i < BITS_IN_UINT; ++i) {
 			res_bits.push_back(0);
 		}
@@ -174,7 +174,7 @@ std::vector<bool> BigInt::get_bits(uint num, bool pad_uint) {
 std::vector<bool> BigInt::get_bits() const {
 	std::vector<bool> res_bits;
 	size_t my_size = this->num_digits();
-	res_bits.reserve(BITS_IN_UINT * my_size); 
+	res_bits.reserve(BITS_IN_UINT * my_size);
 
 	for (size_t i = 0; i < my_size; ++i) {
 		std::vector<bool> bit_block_32 = get_bits(this->digits[i], (i < my_size - 1));
@@ -184,7 +184,7 @@ std::vector<bool> BigInt::get_bits() const {
 	return res_bits;
 }
 
-// get number of base 2^32 digits in BigInt
+// get number of base 2^32 (BASE) digits in BigInt
 size_t BigInt::num_digits() const {
 	return this->digits.size();
 }
@@ -201,7 +201,7 @@ BigInt BigInt::add_like_signs(const BigInt& a, const BigInt& b) {
 	ulong overflow = 0;
 	while (i < a_num_digits && j < b_num_digits) { // add a's and b's digits pair-wise + carry
 		ulong true_add = (ulong)a.digits[i++] + b.digits[j++] + overflow;
-		uint new_digit = (uint)true_add; // gives result mod 2^32
+		uint new_digit = (uint)true_add; // gives result mod BASE
 		overflow = true_add >> BITS_IN_UINT; // 1 if true_add > BASE - 1 else 0
 		result_digits.push_back(new_digit);
 	}
@@ -210,7 +210,7 @@ BigInt BigInt::add_like_signs(const BigInt& a, const BigInt& b) {
 	// iterate through remainder of a's digits, adding possible carries
 	while (i < a_num_digits) {
 		ulong true_add = (ulong)a.digits[i++] + overflow;
-		uint new_digit = (uint)true_add; // gives result mod 2^32
+		uint new_digit = (uint)true_add; // gives result mod BASE
 		overflow = true_add >> BITS_IN_UINT; // 1 if true_add > BASE - 1 else 0
 		result_digits.push_back(new_digit);
 	}
@@ -219,7 +219,7 @@ BigInt BigInt::add_like_signs(const BigInt& a, const BigInt& b) {
 	// iterate through remainder of b's digits, adding possible carries
 	while (j < b_num_digits) {
 		ulong true_add = (ulong)b.digits[j++] + overflow;
-		uint new_digit = (uint)true_add; // gives result mod 2^32
+		uint new_digit = (uint)true_add; // gives result mod BASE
 		overflow = true_add >> BITS_IN_UINT; // 1 if true_add > BASE - 1 else 0
 		result_digits.push_back(new_digit);
 	}
@@ -243,29 +243,29 @@ BigInt BigInt::add_diff_signs(const BigInt& big, const BigInt& small) {
 	size_t j = 0;
 	uint underflow = 0;
 	while (i < big_num_digits && j < small_num_digits) { // iterate pairwise through a's and b's digits, subtracting - underflow
-		uint new_digit = big.digits[i] - small.digits[j] - underflow; // gives result mod 2^32
+		uint new_digit = big.digits[i] - small.digits[j] - underflow; // gives result mod BASE
 		underflow = (big.digits[i++] < small.digits[j++]) ? 1 : 0;
 		result_digits.push_back(new_digit);
 	}
 
 	// finish off subtracting underflow from big's remaining digits
 	while (i < big_num_digits) {
-		uint new_digit = big.digits[i++] - underflow; // gives result mod 2^32
+		uint new_digit = big.digits[i++] - underflow; // gives result mod BASE
 		underflow = 0;
 		result_digits.push_back(new_digit);
 	}
 
 	// finish off subtracting underflow from small's remaining digits
 	while (j < small_num_digits) {
-		uint new_digit = small.digits[j++] - underflow; // gives result mod 2^32
+		uint new_digit = small.digits[j++] - underflow; // gives result mod BASE
 		underflow = 0;
 		result_digits.push_back(new_digit);
 	}
 
 	// remove possible zero-digits in MSB positions from result
-	for (i = result_digits.size() - 1; i > 0; --i) {
-		if (result_digits[i] != 0) {
-			result_digits.resize(i + 1);
+	for (int ind = result_digits.size() - 1; ind >= 0; --ind) {
+		if (result_digits[ind] != 0) {
+			result_digits.resize(ind + 1);
 			break;
 		}
 	}
@@ -273,7 +273,7 @@ BigInt BigInt::add_diff_signs(const BigInt& big, const BigInt& small) {
 	return BigInt(result_digits, big.positive);
 }
 
-// performs long multiplication (gradeschool multiplication) on two BigInts a and b 
+// performs long multiplication (gradeschool multiplication) on two BigInts a and b, complexity O(n^2)
 BigInt BigInt::long_mult(const BigInt& a, const BigInt& b) {
 	bool sign = !(a.positive ^ b.positive); // just think of truth table for mult of neg and pos
 	const BigInt* longer = &a;
@@ -296,8 +296,8 @@ BigInt BigInt::long_mult(const BigInt& a, const BigInt& b) {
 		// multiply each digit of top BigNum by single digit of bottom BigNum
 		for (size_t j = 0; j < longer_size; ++j) {
 			ulong true_mult = ((ulong)shorter->digits[i] * longer->digits[j]) + overflow; // yes, 2^64-1 > (2^32-1)^2 + (2^32-1)
-			uint new_digit = (uint)true_mult; // gives result mod 2^32
-			overflow = true_mult >> BITS_IN_UINT; // overflow = true_mult / (2^32)
+			uint new_digit = (uint)true_mult; // gives result mod BASE
+			overflow = true_mult >> BITS_IN_UINT; // overflow = true_mult / BASE
 			add_digits.push_back(new_digit);
 		}
 
@@ -313,10 +313,94 @@ BigInt BigInt::long_mult(const BigInt& a, const BigInt& b) {
 	return result;
 }
 
-// todo
-BigInt BigInt::karatsuba_mult(const BigInt& a, const BigInt& b) {
-	return BigInt();
+// performs long multiplication (gradeschool multiplication) on two BigInts a and b, complexity O(n^(log(3)))
+BigInt BigInt::karatsuba_mult(const BigInt& x, const BigInt& y) {
+	const size_t x_n = x.digits.size();
+	const size_t y_n = y.digits.size(); 
+
+	if (x_n < 50 || y_n < 50) {
+		return long_mult(x, y); // karatsuba overhead not worth it, just do long multiplication
+	}
+
+	if (x_n == y_n) {
+		const size_t m = (x_n / 2) + (x_n % 2); // x_n/2 if x_n is even, (x_n+1)/2 if x_n is odd
+		const BigInt a(std::vector<uint>(x.digits.begin() + m, x.digits.end()), true);   // initialize a to left half of x
+		const BigInt b(std::vector<uint>(x.digits.begin(), x.digits.begin() + m), true); // initialize b to right half of x
+		const BigInt c(std::vector<uint>(y.digits.begin() + m, y.digits.end()), true);   // initialize c to left half of y
+		const BigInt d(std::vector<uint>(y.digits.begin(), y.digits.begin() + m), true); // initialize d to right half of y
+		
+		BigInt ac = karatsuba_mult(a, c);
+		BigInt bd = karatsuba_mult(b, d);
+		BigInt ad_plus_bc = karatsuba_mult(a + b, c + d) - ac - bd; // (a+b)(c+d)=ac+ad+bc+bd ==> ac+ad+bc+bd-ac-bd=ad+bc
+
+		if (ac > 0) ac.digits.insert(ac.digits.begin(), 2 * m, 0); // multiply by BASE^(2*m)
+		if (ad_plus_bc > 0) ad_plus_bc.digits.insert(ad_plus_bc.digits.begin(), m, 0); // multiply by BASE^m
+
+		return ac + ad_plus_bc + bd;
+	}
+	else {
+		const BigInt* biggest = &x;
+		const BigInt* smallest = &y;
+		if (biggest->digits.size() < smallest->digits.size()) { // x and y have a different number of digits, need to know which number has least digits
+			std::swap(biggest, smallest);
+		}
+		BigInt padded(*smallest);
+		padded.digits.insert(padded.digits.end(), biggest->digits.size() - smallest->digits.size(), 0); // pad the smaller number with leading 0s
+		return karatsuba_mult(*biggest, padded); // restart karatsuba with our now same-length inputs
+	}
 }
+
+//BigInt BigInt::karatsuba_mult(const BigInt& x, const BigInt& y) {
+//	const size_t x_n = x.num_digits();
+//	const size_t y_n = y.num_digits();
+//	if (x_n < 75 || y_n < 75) {
+//		return long_mult(x, y); // karatsuba overhead not worth it, just do grade-school multiplication
+//	}
+//
+//	const size_t n = std::max(x_n, y_n);
+//	const size_t m = (n / 2) + (n % 2); // n/2 if n is even, (n+1)/2 if n is odd
+//
+//	BigInt a, b, c, d;
+//	a.digits.reserve(m + 1); b.digits.reserve(m + 1); c.digits.reserve(m + 1); d.digits.reserve(m + 1);
+//	size_t x_cnt = 0;
+//	size_t y_cnt = 0;
+//
+//	while (x_cnt < m && x_cnt < x_n) { // initialize b to m least significant digits of x
+//		if (x_cnt == 0) {
+//			b.digits.pop_back();
+//		}
+//		b.digits.push_back(x.digits[x_cnt++]);
+//	}
+//	while (x_cnt < x_n) { // initialize a to remaining most significant digits of x
+//		if (x_cnt == m) {
+//			a.digits.pop_back();
+//		}
+//		a.digits.push_back(x.digits[x_cnt++]);
+//	}
+//	while (y_cnt < m && y_cnt < y_n) { // initialize c to m least significant digits of y
+//		if (y_cnt == 0) {
+//			d.digits.pop_back();
+//		}
+//		d.digits.push_back(y.digits[y_cnt++]);
+//	}
+//	while (y_cnt < y_n) { // initialize d to remaining most significant digits of y
+//		if (y_cnt == m) {
+//			c.digits.pop_back();
+//		}
+//		c.digits.push_back(y.digits[y_cnt++]);
+//	}
+//
+//	// if x = a*BASE^m + b and y=c*BASE^m + d
+//	// then xy = ac*BASE^(2m) + (ad+bc)*BASE^m + bd
+//	BigInt ac = karatsuba_mult(a, c);
+//	BigInt bd = karatsuba_mult(b, d);
+//	BigInt ad_plus_bc = karatsuba_mult(a + b, c + d) - ac - bd; // (a+b)(c+d)=ac+ad+bc+bd ==> ac+ad+bc+bd-ac-bd=ad+bc
+//
+//	ac.digits.insert(ac.digits.begin(), 2*m, 0); // multiply ac by BASE^(2m)
+//	ad_plus_bc.digits.insert(ad_plus_bc.digits.begin(), m, 0); // multiply (ad+bc) by BASE^m
+//
+//	return ac + ad_plus_bc + bd;
+//}
 
 // todo
 BigInt BigInt::schonhage_strassen_mult(const BigInt& a, const BigInt& b) {
@@ -359,7 +443,7 @@ bool BigInt::operator== (const BigInt& right) const {
 	if (my_size == right.num_digits()) { // same # of digits, ok...
 		if (this->positive == right.positive) { // hmm, signs are the same too...
 			for (size_t i = 0; i < my_size; ++i) {
-				if (digits[i] != right.digits[i]) { 
+				if (digits[i] != right.digits[i]) {
 					return false; // nevermind, we have a different digit so we're different numbers
 				}
 			}
@@ -368,7 +452,7 @@ bool BigInt::operator== (const BigInt& right) const {
 	}
 	return false;
 }
- 
+
 // is this < right?
 bool BigInt::operator< (const BigInt& right) const {
 	size_t my_size = this->num_digits();
@@ -430,14 +514,19 @@ BigInt BigInt::operator+ (const BigInt& right) const {
 
 	// diff signs
 	BigInt me(*this);
-	bool positive_save = me.positive;
-	me.positive = right.positive;
+	BigInt them(right);
+	bool my_sign = me.positive;
+	bool their_sign = them.positive;
+	me.positive = true;
+	them.positive = true;
 	if (me < right) { // compare magnitude of this and right
-		me.positive = positive_save;
-		return add_diff_signs(right, me); // want to send higher mag BigNum as first arg
+		me.positive = my_sign;
+		them.positive = their_sign;
+		return add_diff_signs(them, me); // want to send higher mag BigNum as first arg
 	}
-	me.positive = positive_save;
-	return add_diff_signs(me, right);
+	them.positive = their_sign;
+	me.positive = my_sign;
+	return add_diff_signs(me, them);
 }
 
 // returns BigInt a where a = this - right
